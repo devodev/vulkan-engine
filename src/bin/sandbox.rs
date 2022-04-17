b1aca988f1e39791ab5ef3f742d65782aae80cab
devodev <abalexandrebarone@gmail.com>
2022-04-17 18:52:21 -0400

    Add engine-icon as window icon in sandbox

--- src/bin/sandbox.rs
+++ src/bin/sandbox.rs
@@ -1,7 +1,10 @@
 use core::EngineBuilder;
 
 use log::LevelFilter;
-use winit::dpi::{LogicalSize, Size};
+use winit::{
+    dpi::{LogicalSize, Size},
+    window::Icon,
+};
 
 fn main() {
     // initialize logger
@@ -10,11 +13,24 @@ fn main() {
         .parse_default_env()
         .init();
 
+    // load window icon
+    let icon_bytes = include_bytes!("../../assets/engine-icon.png");
+    let icon_image = image::load_from_memory(icon_bytes).expect("failed to load image from bytes");
+    let icon = match Icon::from_rgba(
+        icon_image.clone().into_bytes(),
+        icon_image.width(),
+        icon_image.height(),
+    ) {
+        Ok(icon) => icon,
+        Err(e) => panic!("failed to load icon from image: {:?}", e),
+    };
+
     // create and run engine
     // engine takes ownership of thread and will call std::process::exit for us
     EngineBuilder::new()
         .with_window_size(Size::Logical(LogicalSize::new(1024.0, 768.0)))
         .with_window_title("Sandbox (Vulkan Engine)".to_owned())
+        .with_window_icon(icon)
         .build()
         .run()
         .expect("failed to run engine");
