52dedea851e3c0c06ffefee2983863e9bfdc8599
devodev <abalexandrebarone@gmail.com>
2022-04-17 15:16:49 -0400

    simplify sandbox main - The event loop used in Engine.run() hijacks the main thread and calls std::process::exit when done, therefore we can remove our own logic.

--- src/bin/sandbox.rs
+++ src/bin/sandbox.rs
@@ -1,29 +1,10 @@
-use log::info;
-use std::error::Error;
-
 use core::Engine;
 
 fn main() {
     // initialize logger
     env_logger::init();
 
-    // run app
-    std::process::exit(match run_app() {
-        Ok(_) => 0,
-        Err(err) => {
-            eprintln!("error: {:?}", err);
-            1
-        }
-    });
-}
-
-fn run_app() -> Result<(), Box<dyn Error>> {
-    info!("start");
-
-    let mut engine = Engine::new();
-    engine.run()?;
-
-    info!("end");
-
-    Ok(())
+    // create and run engine
+    // engine takes ownership of thread and will call std::process::exit for us
+    Engine::new().run().unwrap();
 }
