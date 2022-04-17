2debb17f1e0247a72e74c86b8f70a1626ec7fa72
devodev <abalexandrebarone@gmail.com>
2022-04-17 15:31:47 -0400

    Set default log level to INFO

--- src/bin/sandbox.rs
+++ src/bin/sandbox.rs
@@ -1,8 +1,13 @@
+use log::LevelFilter;
+
 use core::Engine;
 
 fn main() {
     // initialize logger
-    env_logger::init();
+    env_logger::Builder::new()
+        .filter_level(LevelFilter::Info)
+        .parse_default_env()
+        .init();
 
     // create and run engine
     // engine takes ownership of thread and will call std::process::exit for us
