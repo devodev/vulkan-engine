a30cad1b616b5924bfeb6edb932f36c3315528c8
devodev <abalexandrebarone@gmail.com>
2022-05-19 23:17:55 -0400

    make Timing better

--- src/debug.rs
+++ src/debug.rs
@@ -1,31 +1,67 @@
-use std::time::Instant;
+use std::{
+    borrow::Cow,
+    time::{Duration, Instant},
+};
 
-use log::debug;
+use log::{debug, error, info, trace, warn};
 
-pub struct Timing {
+// inspired from: https://gitlab.com/imp/easytiming-rs/-/blob/master/src/lib.rs
+pub struct Timing<'a> {
     start: Instant,
-    msg: &'static str,
+    level: log::Level,
+    msg: Cow<'a, str>,
 }
 
-impl Timing {
-    pub fn new(msg: &'static str) -> Self {
+impl<'a> Default for Timing<'a> {
+    fn default() -> Self {
         Self {
             start: Instant::now(),
-            msg,
+            level: log::Level::Trace,
+            msg: "TIME!".into(),
         }
     }
 }
 
-impl Drop for Timing {
+impl<'a> Timing<'a> {
+    pub fn new<N>(msg: N) -> Self
+    where
+        N: Into<Cow<'a, str>>,
+    {
+        let mut t = Self::default();
+        t.msg = msg.into();
+        t
+    }
+
+    #[inline]
+    fn elapsed(&self) -> Duration {
+        Instant::now().duration_since(self.start)
+    }
+
+    #[inline]
+    fn log(&self) {
+        let msg = format!("[{:?}] {}", self.elapsed(), self.msg);
+        match self.level {
+            log::Level::Error => error!("{}", msg),
+            log::Level::Warn => warn!("{}", msg),
+            log::Level::Info => info!("{}", msg),
+            log::Level::Debug => debug!("{}", msg),
+            log::Level::Trace => trace!("{}", msg),
+        }
+    }
+}
+
+impl<'a> Drop for Timing<'a> {
     fn drop(&mut self) {
-        let elapsed = Instant::now().duration_since(self.start);
-        debug!("{}", format!("[{:?}] {}", elapsed, self.msg))
+        self.log()
     }
 }
 
 #[macro_export]
 #[cfg(debug_assertions)]
 macro_rules! TIME {
+    () => {
+        let _x = $crate::debug::Timing::new();
+    };
     ($msg: expr) => {
         let _x = $crate::debug::Timing::new($msg);
     };
@@ -33,7 +69,41 @@ macro_rules! TIME {
 #[macro_export]
 #[cfg(not(debug_assertions))]
 macro_rules! TIME {
+    () => {
+        ()
+    };
     ($msg: expr) => {
         ()
     };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const MSG: &str = "timing";
+
+    #[test]
+    fn fromstr() {
+        let t: Timing = Timing::new(MSG);
+        assert_eq!(t.msg, MSG);
+    }
+
+    #[test]
+    fn fromstring() {
+        let t: Timing = Timing::new(String::from(MSG));
+        assert_eq!(t.msg, MSG);
+    }
+
+    #[test]
+    fn fromborrowed() {
+        let t: Timing = Timing::new(Cow::Borrowed(MSG));
+        assert_eq!(t.msg, MSG);
+    }
+
+    #[test]
+    fn fromowned() {
+        let t: Timing = Timing::new(Cow::Owned(String::from(MSG)));
+        assert_eq!(t.msg, MSG);
+    }
 }
